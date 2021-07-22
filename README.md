9e0d47ef345e2f135092e2ba48d5a3aa480ce28f
EmacsPenguin <you@example.com>
2021-07-22 01:09:23 -0400

    modified:   README.md 	modified:   minecraft-config-script.sh

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # minecraft-autoconfiguration-script
-This is a script that automates setting up a minecraft server. It is still a work in progress, but the 1.12.2 vanilla setup is working flawlessly.
+This is a script that automates setting up a minecraft server. It is still a work in progress, but the 1.12.2 and 1.16.5 vanilla setup is working flawlessly.
+
+## dependencies 
+
+You need the following programs to use the script.
+
+* openjdk-8-jdk
+* dialog
+* bash
+
+## what to do after running script
+
+You will need to set up networking for your minecraft server. This includes port forwarding and opening your firewall. For more information you should consult [the wiki](https://minecraft.fandom.com/wiki/Tutorials/Setting_up_a_server#Port_forwarding).
+
+## things not working and wanted features
+
+* Forge is not working right now.
+* Support for 1.17.2, and 1.8.9
+* Support for Forge and Spigot
--- minecraft-config-script.sh
+++ minecraft-config-script.sh
@@ -64,7 +64,7 @@ vanilla_12_install () {
 	clear
 	echo "Downloading Minecraft"
 	echo "-------------------------------------"
-	wget -O minecraft-server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/1.16.5/minecraft_server.1.16.5.jar"
+	wget -O minecraft-server.jar "https://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar"
 	chmod +x minecraft-server.jar
 
 	dialog --title "Your Done!" --msgbox "To run your minecraft server go to the Minecraft-Server directory and run start.sh. You will need to manually configure your networking. You may need to open your firewall, and portforword the server. You can use Gnu Screen or another terminal multiplexer to run your server without keeping your terminal open." 7 60
@@ -88,7 +88,7 @@ minecraft_version_12 () {
 }
 
 minecraft_version_16 () {
-	dialog --title "1.16.5" --msgbox "You chose 1.16.5 in the next menu you can select what type of server you would like." 9 60
+	dialog --title "1.16.4" --msgbox "You chose 1.16.4 in the next menu you can select what type of server you would like." 9 60
 	dialog --title "What server would you like to use." --menu "" 15 70 2 Vanilla "The official minecraft server." Forge "Is the best for mods" 2> /tmp/minecraft_autoconfig_dialog2
 
 	sub_version=$(cat /tmp/minecraft_autoconfig_dialog2)
@@ -106,14 +106,14 @@ minecraft_version_16 () {
 
 echo "If there is an error message you may need to install dialog. It should be in most distro's repos."
 dialog --title "Welcome" --msgbox "This is a Minecraft server auto configuration script. Before you can begin you need to install the following dependencies: openjdk-8-jdk, and wget. Answer the following questions and you should end up with a functioning Minecraft server." 9 60
-dialog --title "What version of minecraft would you like to use." --menu "" 15 95 4 1.16.5 "The most recent version supported by this script, but it is bad for modded" 1.12.2 "The best version from modded minecraft." 2> /tmp/minecraft_autoconfig_dialog1
+dialog --title "What version of minecraft would you like to use." --menu "" 15 95 4 1.16.4 "The most recent version supported by this script, but it is bad for modded" 1.12.2 "The best version from modded minecraft." 2> /tmp/minecraft_autoconfig_dialog1
 
 output=$(cat /tmp/minecraft_autoconfig_dialog1)
 
 if [ "$output" == "1.12.2" ]
 then
 	minecraft_version_12
-elif [ "$output" == 1.16.5 ]
+elif [ "$output" == 1.16.4 ]
 then
 	minecraft_version_16
 fi
