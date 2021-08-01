b51b2c28aad9475d4f2bf82dc759caaef7fbf484
EmacsPenguin <you@example.com>
2021-08-01 17:19:00 -0400

    modified:   minecraft-config-script.sh

--- minecraft-config-script.sh
+++ minecraft-config-script.sh
@@ -1,7 +1,86 @@
 #!/bin/bash
 
+paper_8_install () {
+
+	dialog --title "This will install paper 1.8.9." --msgbox "This will install paper 1.18.9" 7 60
+
+	mkdir Minecraft-Server
+	cd Minecraft-Server
+
+	total_ram=$(free -m | awk '{print $2}' | sed -n 2p)
+	ram_message="How much ram (in megabytes) would you like to dedicate to the server. You have ""$total_ram"" in total."
+	
+	dialog --title "Dedicated Ram" --inputbox "$ram_message" 7 60 2> /tmp/minecraft_autoconfig_dialog3
+	dialog --title "Eula" --yesno "Do you except the minecraft EULA?" 7 60
+
+	if [ $? == 0 ]
+	then
+		echo eula=true > eula.txt
+	else
+		dialog --title "EULA" --msgbox "Sorry you need to execpt the EULA!"
+	fi
+	
+	dedicated_ram=$(cat /tmp/minecraft_autoconfig_dialog3)
+	full_start_command="java -Xmx""$dedicated_ram""M -Xms1024M -jar minecraft-server.jar nogui"
+	
+	echo "$full_start_command" > start.sh
+	chmod +x start.sh
+
+	rm /tmp/minecraft_autoconfig_dialog3
+
+	clear
+	echo "Downloading Minecraft"
+	echo "-------------------------------------"
+	wget -O minecraft-server.jar "https://papermc.io/api/v2/projects/paper/versions/1.8.8/builds/443/downloads/paper-1.8.8-443.jar"
+	chmod +x minecraft-server.jar
+
+	dialog --title "Your Done!" --msgbox "To run your minecraft server go to the Minecraft-Server directory and run start.sh. You will need to manually configure your networking. You may need to open your firewall, and portforword the server. You can use Gnu Screen or another terminal multiplexer to run your server without keeping your terminal open." 9 60
+
+}
+
+
+vanilla_8_install () {
+
+	dialog --title "This will install vanilla 1.8.9." --msgbox "This will install vanilla 1.18.9" 7 60
+
+	mkdir Minecraft-Server
+	cd Minecraft-Server
+
+	total_ram=$(free -m | awk '{print $2}' | sed -n 2p)
+	ram_message="How much ram (in megabytes) would you like to dedicate to the server. You have ""$total_ram"" in total."
+	
+	dialog --title "Dedicated Ram" --inputbox "$ram_message" 7 60 2> /tmp/minecraft_autoconfig_dialog3
+	dialog --title "Eula" --yesno "Do you except the minecraft EULA?" 7 60
+
+	if [ $? == 0 ]
+	then
+		echo eula=true > eula.txt
+	else
+		dialog --title "EULA" --msgbox "Sorry you need to execpt the EULA!"
+	fi
+	
+	dedicated_ram=$(cat /tmp/minecraft_autoconfig_dialog3)
+	full_start_command="java -Xmx""$dedicated_ram""M -Xms1024M -jar minecraft-server.jar nogui"
+	
+	echo "$full_start_command" > start.sh
+	chmod +x start.sh
+
+	rm /tmp/minecraft_autoconfig_dialog3
+
+	clear
+	echo "Downloading Minecraft"
+	echo "-------------------------------------"
+	wget -O minecraft-server.jar "https://launcher.mojang.com/v1/objects/b58b2ceb36e01bcd8dbf49c8fb66c55a9f0676cd/server.jar"
+	chmod +x minecraft-server.jar
+
+	dialog --title "Your Done!" --msgbox "To run your minecraft server go to the Minecraft-Server directory and run start.sh. You will need to manually configure your networking. You may need to open your firewall, and portforword the server. You can use Gnu Screen or another terminal multiplexer to run your server without keeping your terminal open." 9 70
+
+
+}
+
+
 forge_16_install () {
-	dialog --title "this would install forge 1.16.4" --msgbox "This would install forge 1.16.4" 7 60
+	dialog --title "this will install forge 1.16.4" --msgbox "This will install forge 1.16.4" 7 60
 
 	mkdir Minecraft-Server
 	cd Minecraft-Server
@@ -40,7 +119,7 @@ forge_16_install () {
 }
 
 forge_12_install () {
-	dialog --title "this would install forge 1.12.2" --msgbox "This would install forge 1.12.2" 7 60
+	dialog --title "this will install forge 1.12.2" --msgbox "This will install forge 1.12.2" 7 60
 
 	mkdir Minecraft-Server
 	cd Minecraft-Server
@@ -84,7 +163,7 @@ vanilla_16_install () {
 	cd Minecraft-Server
 
 	total_ram=$(free -m | awk '{print $2}' | sed -n 2p)
-	ram_message="How much ram (in megabytes) would you like to dedicate to the server. You have ""$total_ram"" in total."
+	ram_message="How much ram (in megabytes) will you like to dedicate to the server. You have ""$total_ram"" in total."
 	
 	dialog --title "Dedicated Ram" --inputbox "$ram_message" 7 60 2> /tmp/minecraft_autoconfig_dialog3
 	dialog --title "Eula" --yesno "Do you except the minecraft EULA?" 7 60
@@ -148,6 +227,24 @@ vanilla_12_install () {
 	dialog --title "Your Done!" --msgbox "To run your minecraft server go to the Minecraft-Server directory and run start.sh. You will need to manually configure your networking. You may need to open your firewall, and portforword the server. You can use Gnu Screen or another terminal multiplexer to run your server without keeping your terminal open." 9 70
 }
 
+minecraft_version_8 () {
+
+	dialog --title "1.8.9" --msgbox "You chose 1.8.9, in the next menu you can select what type of server you would like. If you are new to running a server I recommend using vanilla." 7 60
+	dialog --title "What server would you like to use." --menu "" 15 70 3 Vanilla "The official minecraft server." Paper "Minecraft with anticheat" 2> /tmp/minecraft_autoconfig_dialog2
+
+	sub_version=$(cat /tmp/minecraft_autoconfig_dialog2)
+	rm /tmp/minecraft_autoconfig_dialog2
+
+	if [ "$sub_version" == "Vanilla" ]
+	then
+		vanilla_8_install
+	elif [ "$sub_version" == "Paper" ]
+	then
+		paper_8_install
+	fi
+
+
+}
 
 minecraft_version_12 () {
 	dialog --title "1.12.2" --msgbox "You chose 1.12.2, in the next menu you can select what type of server you would like. If you are new to running a server I recommend using vanilla." 7 60
@@ -184,7 +281,7 @@ minecraft_version_16 () {
 
 echo "If there is an error message you may need to install dialog. It should be in most distro's repos."
 dialog --title "Welcome" --msgbox "This is a Minecraft server auto configuration script. Before you can begin you need to install the following dependencies: openjdk-8-jdk, and wget. Answer the following questions and you should end up with a functioning Minecraft server." 9 60
-dialog --title "What version of minecraft would you like to use." --menu "" 15 95 4 1.16.4 "The most recent version supported by this script, but it is bad for modded" 1.12.2 "The best version from modded minecraft." 2> /tmp/minecraft_autoconfig_dialog1
+dialog --title "What version of minecraft would you like to use." --menu "" 15 95 4 1.16.4 "The most recent version supported by this script, but it is bad for modded" 1.12.2 "The best version from modded minecraft." 1.8.9 "The best for PVP" 2> /tmp/minecraft_autoconfig_dialog1
 
 output=$(cat /tmp/minecraft_autoconfig_dialog1)
 
@@ -194,6 +291,9 @@ then
 elif [ "$output" == 1.16.4 ]
 then
 	minecraft_version_16
+elif [ "$output" == 1.8.9 ]
+then
+	minecraft_version_8
 fi
 
 rm /tmp/minecraft_autoconfig_dialog1
